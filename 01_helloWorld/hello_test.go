4b2554f20b7ea167b39ef86c526cb9a0c2a2e99f
Aviral <aviralji4@gmail.com>
2025-05-05 22:39:13 +0530

    Improve error message formatting in TestHello

--- 01_helloWorld/hello_test.go
+++ 01_helloWorld/hello_test.go
@@ -6,6 +6,6 @@ func TestHello(t *testing.T) {
 	got := Hello()
 	want := "Hello, World!"
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got: %q, wanted: %q", got, want)
 	}
 }
