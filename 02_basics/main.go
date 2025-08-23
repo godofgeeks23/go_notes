c053fcda42ac2c222dc9995c89aac3bd5462431b
Aviral <aviralji4@gmail.com>
2025-08-23 22:23:27 +0530

    Update main.go

--- 02_basics/main.go
+++ 02_basics/main.go
@@ -1,35 +1,65 @@
 /*
-go is fast in execution than interpreted langs
-go is fast in compilation than compiled langs
-go is not fast than natively compiled langs in execution
-go is natively compiled but its exec speed is similar to VM Langs (java, c#) due to go runtime
-go is strongly and statically typed
-go is garbage collected
-go is not a pure OOP language
 
-Numeric data types in go -
-- signed
-- unsigned
-- float
- - complex
+execution times: interpreted lang > go (due to go-runtime), VM based lang(java, c#) > natively compiled lang
+compilation time: go > compiled lang
+
+- strongly and statically typed
+- garbage collected
+- not a pure OOP language
+
+Basic data types in go -
+bool
+string
+
+int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr
+byte // alias for uint8
+rune // alias for int32, represents a Unicode code point
+float32 float64
+complex64 complex128
+
 */
 
-package main
+package main // every go program is made up of packages
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 // variables in go are always initialized to their zero value if not explicitly initialized
-var c, python, java bool
+var c, python, java bool // var declares a list of variables in an scope
 
-var k int = 8 // this is valid, k is initialized to 8
+var k, z int = 8, 10 // this is valid, k is initialized to 8
 // k:= 8 - this is not valid in package scope. can only be done in function scope
+
+const Pi = 3.14
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x int, y int) (int, int) {
+	return y, x
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("My favorite number is", rand.Intn(10))
 	i := 42
 	fmt.Println(i, c, python, java)
+	fmt.Println("pi =", math.Pi) // a name is exported if it begins with a capital letter
+
+	x := 12
+	y := 78
+	fmt.Println("sum =", add(x, y))
+	fmt.Printf("%v, %v\n", x, y)
+	x, y = swap(x, y)
+	fmt.Printf("%v, %v\n", x, y)
+
+	var f float64 = float64(i) // The expression T(v) converts the value v to the type T
+	fmt.Println(f)
 }
 
-// summmary:
 // var i int - use this when initial value does not matter
 // i := 42 - use this when initial value is known
