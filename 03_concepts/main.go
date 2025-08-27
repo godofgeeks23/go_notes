6775f6a4881beb53536f84a55ee2f270c5df0d6f
Aviral <aviralji4@gmail.com>
2025-08-27 21:55:10 +0530

    Create main.go

---
+++ 03_concepts/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("sum", sum)
+
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	// infinite loop
+	// for {
+	// }
+
+	// Like for, the if statement can start with a short statement to execute before the condition
+
+}
