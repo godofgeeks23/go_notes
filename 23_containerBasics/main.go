717ece4546037e3b14a95bd218c409ff5a45a704
Aviral <aviralji4@gmail.com>
2025-12-01 17:05:57 +0530

    Create main.go

---
+++ 23_containerBasics/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+/*
+container primary components-
+namespaces - what a process can see. created with syscalls. restricting the view of the container of host machine
+change groups - chroot
+control groups - cgroup - what you can use, filesystem interface
+*/
+
+func run() {
+	fmt.Printf("[run()] running: %v (PID:%v)\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// this will create a new namespace for the process of its own so that it can't see what going on in the parent host
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("[run()] error:", err)
+		panic(err)
+	}
+}
+
+func child() {
+	fmt.Printf("[child()] running %v (PID:%v)\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("container-fs/filesystem")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("[child()] error:", err)
+		panic(err)
+	}
+
+	defer syscall.Unmount("/proc", 0)
+}
+
+func main() {
+	fmt.Println("container demo")
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("bad command")
+	}
+}
