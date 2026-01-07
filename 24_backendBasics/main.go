c880cd3a9d606782d1c428b86d013a2f7f44c238
Aviral <aviralji4@gmail.com>
2026-01-07 23:35:05 +0530

    Update main.go

--- 24_backendBasics/main.go
+++ 24_backendBasics/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+type Response struct {
+	Error   bool   `json:"error"`
+	Data    any    `json:"data,omitempty"`
+	Message string `json:"message"`
+}
+
+// logging middleware
 func requestLogger(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		next(w, r)
-		fmt.Printf("\n%s %s %s %s",
+		fmt.Printf("%s %s %s %s\n",
 			r.Method,
 			r.RequestURI,
 			r.RemoteAddr,
@@ -18,15 +32,63 @@ func requestLogger(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from server")
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var u User
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	fmt.Println("received: ", u)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	resp := Response{
+		Error:   false,
+		Message: "Success",
+		Data:    &u,
+	}
+	json.NewEncoder(w).Encode(resp)
+}
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	dog := struct {
+		Name   string `json:"name"`
+		IsGood bool   `json:"good"`
+	}{
+		"Rex",
+		true,
+	}
+
+	resp := Response{
+		Error:   false,
+		Message: "Succes",
+		Data:    &dog,
+	}
+	json.NewEncoder(w).Encode(resp)
 }
 
 var Port string = ":3000"
 
 func main() {
-	fmt.Println("hellow world")
-	http.HandleFunc("/", requestLogger(root))
+	http.HandleFunc("/", requestLogger(rootHandler))
+	http.HandleFunc("/user", requestLogger(userHandler))
+	http.HandleFunc("/random", requestLogger(randomHandler))
 	fmt.Println("Server starting at " + Port)
 
 	err := http.ListenAndServe(Port, nil)
